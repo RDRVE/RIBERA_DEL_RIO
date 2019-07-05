3dd577a3f020be1cc63a2c4eb75f4a4ec6d19b67
Samir pazo <programador.pazo@gmai.com>
2019-07-05 16:58:54 -0500

    swap

---
+++ DataAccess/DaConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class DaConnection
+    {
+
+        #region Atributes
+        private string connectionString;
+        #endregion
+
+        #region Properties
+        public string ConnectionString
+        {
+            get
+            {
+                return this.connectionString;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public DaConnection()
+        {
+            this.connectionString = "";
+        }
+        #endregion
+    }
+}
